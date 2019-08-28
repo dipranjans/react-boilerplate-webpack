0f8db53f0d5a882febcff0e20797185b9fd86be3
Diptiranjan Sukla <sukla.dipti@gmail.com>
2019-08-28 11:01:34 +0530

    react boilerplatewith babel webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+let HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src/")
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "src"),
+    historyApiFallback: false,
+    compress: true,
+    port: 9000
+  },
+  externals: {
+    //config: JSON.stringify({
+    //apiUrl: "http://localhost:4000"
+    //})
+  }
+};
